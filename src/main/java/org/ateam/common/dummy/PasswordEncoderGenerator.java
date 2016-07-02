bac3d4ae0b31730b46ea51c500bb816eafbf4c90
Jeremiah Lumontod <jeremiahlumontod@Jeremiahs-MBP.Home>
2016-07-02 23:38:46 +0400

    first commit

---
+++ src/main/java/org/ateam/common/dummy/PasswordEncoderGenerator.java
@@ -0,0 +1,23 @@
+package org.ateam.common.dummy;
+
+/**
+ * Created by jml on 6/21/16.
+ */
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+
+public class PasswordEncoderGenerator {
+
+    public static void main(String[] args) {
+
+        int i = 0;
+        while (i < 10) {
+            String password = "123456";
+            BCryptPasswordEncoder passwordEncoder = new BCryptPasswordEncoder();
+            String hashedPassword = passwordEncoder.encode(password);
+
+            System.out.println(hashedPassword);
+            i++;
+        }
+
+    }
+}
